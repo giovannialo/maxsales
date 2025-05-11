f4497e02ec3e63f566c00f2293df5e0ee79f4d86
giovannialo <giovanni.al.oliveira@gmail.com>
2025-05-11 20:34:55 -0300

    feat: adicionar página /about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<div class="mx-auto max-w-lg text-center">
+	<h1 class="text-3xl font-semibold sm:text-2xl">About</h1>
+	<p class="mt-2 text-zinc-600">
+		This is a simple SvelteKit application that demonstrates the use of TypeScript and Tailwind CSS.
+		It includes a basic layout and a few pages to showcase the framework's capabilities.
+	</p>
+</div>

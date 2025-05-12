c50194bd12df3fa3d46ec27a5612cd769fd6b87f
giovannialo <giovanni.al.oliveira@gmail.com>
2025-05-12 02:56:05 -0300

    chore: alterar configuração do adapter

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,9 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '/'
+			fallback: '/index.html',
+			precompress: false,
+			strict: true
 		}),
 		serviceWorker: {
 			register: false
